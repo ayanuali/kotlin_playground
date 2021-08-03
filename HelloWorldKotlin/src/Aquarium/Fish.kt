15a0538c22ff7f1776c6a22619e766da069a0992
Ayan Uali <ayan@freshop.com>
2021-08-03 15:52:12 +0600

    init blocks

--- HelloWorldKotlin/src/Aquarium/Fish.kt
+++ HelloWorldKotlin/src/Aquarium/Fish.kt
@@ -5,7 +5,9 @@ package Aquarium
 class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
     val size: Int
 
-    
+    init {
+        println("first init block")
+    }
 
     init {
         if(friendly) {
