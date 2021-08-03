c7a796ae62c72437ad504bef0f4cca4b1a80c364
Ayan Uali <ayan@freshop.com>
2021-08-03 14:41:50 +0600

    class with default constructor

--- HelloWorldKotlin/src/Aquarium/Fish.kt
+++ HelloWorldKotlin/src/Aquarium/Fish.kt
@@ -1,6 +1,22 @@
 package Aquarium
 
 //volumeNeeded does not have val or var, so kotlin won't create a property for you
+//kotlin creates constructor from this information
 class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
+    val size: Int
 
+    
+
+    init {
+        if(friendly) {
+            size = volumeNeeded
+        } else {
+            size = volumeNeeded * 2
+        }
+    }
+
+    fun fishExample() {
+        val fish = Fish(true, 20)
+        println("Is this fish friendly? ${fish.friendly}. It needs volume ${fish.size}")
+    }
 }
