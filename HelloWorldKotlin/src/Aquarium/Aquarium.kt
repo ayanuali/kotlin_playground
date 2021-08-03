d4fcbf4b7203ce179120d9027d6c5fc8f48a3679
Ayan Uali <ayan@freshop.com>
2021-08-03 16:09:39 +0600

    class inheritance

--- HelloWorldKotlin/src/Aquarium/Aquarium.kt
+++ HelloWorldKotlin/src/Aquarium/Aquarium.kt
@@ -1,6 +1,8 @@
 package Aquarium
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import kotlin.math.PI
+
+open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     //val makes the instance variables immutable, meaning you can't change once they have been assigned a value
     //member variables
 //    var width: Int = 20
@@ -8,17 +10,28 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
 //    var length: Int = 100
 
     //properties get initialized in the order which they defined
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         //make it private if you don't want anyone to use this outside of this class
         //everything is public by defaul
-        set(value) { height = (value*1000) / (width *length) }
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
-    constructor(numberOfPish: Int): this() {
+    constructor(numberOfPish: Int) : this() {
         val water = numberOfPish * 2000
-        val tank = water + water*0.1
+        val tank = water + water * 0.1
         height = (tank / (length * width)).toInt()
     }
+}
+
+class TowerTank() : Aquarium() {
+    override var water = volume * 0.8
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 }
