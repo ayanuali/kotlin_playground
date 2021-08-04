1ca28ab415b3a06aade18d1e2a2993a036f750c4
Ayan Uali <ayan@freshop.com>
2021-08-04 19:36:34 +0600

    interfaces

--- HelloWorldKotlin/src/Aquarium/AquariumFish.kt
+++ HelloWorldKotlin/src/Aquarium/AquariumFish.kt
@@ -4,10 +4,20 @@ abstract class AquariumFish() {
     abstract val color: String
 }
 
-class Shark : AquariumFish() {
+class Shark : AquariumFish(), FishAction {
     override val color = "gray"
+    override fun eat() {
+        println("eat() function from Shark")
+    }
 }
 
-class Plecostomus : AquariumFish() {
+class Plecostomus : AquariumFish(), FishAction {
     override val color = "gold"
+    override fun eat() {
+        println("eat function from Plecostomus")
+    }
+}
+
+interface FishAction {
+    fun eat()
 }
--- HelloWorldKotlin/src/Aquarium/Main.kt
+++ HelloWorldKotlin/src/Aquarium/Main.kt
@@ -2,6 +2,7 @@ package Aquarium
 
 fun main(args: Array<String>) {
     buildAquarium()
+    makeFish()
 }
 
 private fun buildAquarium() {
@@ -33,5 +34,10 @@ private fun buildAquarium() {
 }
 
 fun makeFish() {
+    val shark = Shark()
+    val plecostomus = Plecostomus()
 
+    println("Shark: ${shark.color} and Plecostomus ${plecostomus.color}")
+    shark.eat()
+    plecostomus.eat()
 }
