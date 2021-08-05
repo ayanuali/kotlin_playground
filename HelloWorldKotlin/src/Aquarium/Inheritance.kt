359fd339254acf7e7be7fdfcc1c06244ca224200
Ayan Uali <ayan@freshop.com>
2021-08-05 21:04:21 +0600

    interface delegation

--- HelloWorldKotlin/src/Aquarium/Inheritance.kt
+++ HelloWorldKotlin/src/Aquarium/Inheritance.kt
@@ -1,21 +1,59 @@
 package Aquarium
 
-interface AquariumAction {
-    fun eat()
-    fun jump()
-    fun clean()
-    fun catchFish()
-    fun swim() {
-        println("swim")
-    }
+//interface AquariumAction {
+//    fun eat()
+//    fun jump()
+//    fun clean()
+//    fun catchFish()
+//    fun swim() {
+//        println("swim")
+//    }
+//}
+//
+//interface FishAction1 {
+//    fun eat()
+//}
+//
+//abstract class AquariumFish1: FishAction1 {
+//    override fun eat() {
+//        println("fish eat function")
+//    }
+//}
+
+fun main(args: Array<String>) {
+    delegate()
 }
 
-interface FishAction1 {
+fun delegate() {
+    val pleco = Plecostomus2()
+    println("fish color ${pleco.color}")
+    pleco.eat()
+}
+
+interface FishAction2 {
     fun eat()
 }
 
-abstract class AquariumFish1: FishAction1 {
+interface FishColor2 {
+    val color: String
+}
+
+class Plecostomus2(fishColor2: FishColor2 = GoldColor):
+    FishAction2 by PrintingFishAction("eata algae"),
+    FishColor2 by GoldColor
+
+object GoldColor: FishColor2 {
+    override val color: String
+        get() = "gold color"
+}
+
+object RedColor: FishColor2 {
+    override val color: String
+        get() = "red color"
+}
+
+class PrintingFishAction(val food: String): FishAction2 {
     override fun eat() {
-        println("fish eat function")
+        println(food)
     }
 }
