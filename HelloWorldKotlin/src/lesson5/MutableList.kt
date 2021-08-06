0554729b3a6782fce4c92d4d4fd2f38764e9db62
Ayan Uali <ayanuali@gmail.com>
2021-08-06 18:39:54 +0600

    mutable maps

--- HelloWorldKotlin/src/lesson5/MutableList.kt
+++ HelloWorldKotlin/src/lesson5/MutableList.kt
@@ -9,4 +9,17 @@ fun main(args: Array<String>) {
     println(symptoms.subList(4, symptoms.size))
     listOf(1,5,3).sum()
     listOf("a", "b", "cc").sumOf { it.length } // 4
+
+    val cures = mutableMapOf("white spots" to "Ich", "red sores" to "hole disease")
+
+    println(cures.get("white spots"))
+    println(cures["white spots"])
+
+    println(cures.getOrDefault("bloating", "sorry I don't know"))
+
+    cures.getOrElse("bloating") { "No cure for this" }
+
+    val inventory = mutableMapOf("fish net" to 1)
+    inventory.put("tank scrubber", 3)
+    inventory.remove("fish net")
 }
