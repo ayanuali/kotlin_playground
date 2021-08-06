3afad456e7bf5fe5a742842877d3ceda225bf0a2
Ayan Uali <ayanuali@gmail.com>
2021-08-06 19:28:50 +0600

    constants quiz

--- HelloWorldKotlin/src/lesson5/Pairs.kt
+++ HelloWorldKotlin/src/lesson5/Pairs.kt
@@ -6,12 +6,29 @@ fun main(args: Array<String>) {
     println("Here is ${title} written by ${author} in ${year}")
 }
 
+const val MAX_NUMBER_BOOKS_TO_BORROW = 4
+
+//object Constants {
+//    const val BASE_URL = "some_url"
+//}
+
 class Book(val title: String, val author: String, val year: String) {
+    companion object {
+        const val BASE_URL = "some_url"
+    }
     fun titleAuthor(): Pair<String, String> {
         return title to author
     }
 
     fun returnTriple(): Triple<String, String, String> {
         return Triple(title, author, year)
+    }
+
+    fun canBorrow(numbBooks: Int) : Boolean {
+        return (numbBooks < MAX_NUMBER_BOOKS_TO_BORROW)
+    }
+
+    fun printUrl() : String {
+        return BASE_URL + title + ".html"
     }
 }
