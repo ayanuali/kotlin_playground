3a936bb1952cfcb825fadc2a8f8a60bd07f8342a
Ayan Uali <ayanuali@gmail.com>
2021-08-07 10:28:15 +0600

    extension functions

--- HelloWorldKotlin/src/lesson5/Pairs.kt
+++ HelloWorldKotlin/src/lesson5/Pairs.kt
@@ -1,9 +1,15 @@
 package lesson5
 
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     val book = Book("X", "Y", "Z")
     val (title, author, year) = book.returnTriple()
     println("Here is ${title} written by ${author} in ${year}")
+
+    val puppy = Puppy()
+    puppy.playWithBook(book)
+    println(pages)
 }
 
 const val MAX_NUMBER_BOOKS_TO_BORROW = 4
@@ -11,6 +17,21 @@ const val MAX_NUMBER_BOOKS_TO_BORROW = 4
 //object Constants {
 //    const val BASE_URL = "some_url"
 //}
+
+var pages = 300
+
+fun Book.weight() : Double {return pages*1.5}
+
+fun Book.tornPages(numberOfTornPages: Int) = if (pages >= numberOfTornPages) pages -= numberOfTornPages else pages = 0
+
+class Puppy {
+    fun playWithBook(book: Book) {
+        val randomPages = Random.nextInt(0,10)
+        println(randomPages)
+        if(pages > 0)
+        book.tornPages(randomPages)
+    }
+}
 
 class Book(val title: String, val author: String, val year: String) {
     companion object {
