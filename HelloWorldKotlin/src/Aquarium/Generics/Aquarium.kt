d3078652690d5dc750ad246cd7a35fc0b5bfbc6d
Ayan Uali <ayanuali@gmail.com>
2021-08-08 20:54:18 +0600

    generic functions

--- HelloWorldKotlin/src/Aquarium/Generics/Aquarium.kt
+++ HelloWorldKotlin/src/Aquarium/Generics/Aquarium.kt
@@ -24,16 +24,33 @@ class LakeWater: WaterSupply(true) {
 
 //Any? means any type and null, if you remove ?, null is not allowed anymore
 class Aquarium<out T: WaterSupply> (val waterSupply: T) {
-    fun addWater() {
+    fun addWater(cleaner: Cleaner<T>) {
         //check, throws exception if water supply needs processing, otherwise it will continue to the next
         //statement
-        check(!waterSupply.needsProcessed) {"water supply needs processed"}
+//        check(!waterSupply.needsProcessed) {"water supply needs processed"}
+        if(waterSupply.needsProcessed) {
+            cleaner.clean(waterSupply)
+        }
 
         println("adding water from $waterSupply")
     }
 }
 
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaner()
+    }
+}
+
 fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+fun <T: WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println("aquarium water is clean ${aquarium.waterSupply.needsProcessed}")
+}
 
 fun genericExample() {
 //    val aquarium = Aquarium<TapWater>(TapWater())
@@ -42,6 +59,9 @@ fun genericExample() {
 //    val aquarium4 = Aquarium(LakeWater())
 //    aquarium4.waterSupply.filter()
 //    aquarium4.addWater()
+//    val cleaner = TapWaterCleaner()
     val aquarium: Aquarium<TapWater> = Aquarium(TapWater())
-    addItemTo(aquarium)
+//    aquarium.addWater(cleaner)
+//    addItemTo(aquarium)
+    isWaterClean(aquarium)
 }
